8b69db237446b991970106983c502925d6639d84
AmandaCallender <58009118+AmandaCallender@users.noreply.github.com>
2020-09-08 21:37:07 -0400

    MVP

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,14 +1,24 @@
 '''
-Input: an integer
-Returns: an integer
+Input: a List of integers as well as an integer `k` representing the size of the sliding window
+Returns: a List of integers
 '''
-def eating_cookies(n):
-    # Your code here
+def sliding_window_max(nums, k):
+    max = 0
+    n = len(nums)
+    arr1 = []
 
-    pass
+    for i in range(n - k + 1):
+        max = nums[i]
+        for j in range(1, k):
+            if nums[i + j] > max:
+                max = nums[i + j]
+        arr1.append(max)
+    return arr1
 
-if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+if __name__ == '__main__':
+    # Use the main function here to test out your implementation 
+    arr = [1, 3, -1, -3, 5, 3, 6, 7]
+    k = 3
+
+    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
