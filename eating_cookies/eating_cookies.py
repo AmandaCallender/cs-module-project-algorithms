98d0768616d29eb11dd05178e29d9ced73f206aa
AmandaCallender <58009118+AmandaCallender@users.noreply.github.com>
2020-09-10 19:45:32 -0400

    Fixed

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,24 +1,24 @@
 '''
-Input: a List of integers as well as an integer `k` representing the size of the sliding window
-Returns: a List of integers
+Input: an integer
+Returns: an integer
 '''
-def sliding_window_max(nums, k):
-    max = 0
-    n = len(nums)
-    arr1 = []
+def eating_cookies(n, cache = None):
+    if n < 0:
+        return 0
 
-    for i in range(n - k + 1):
-        max = nums[i]
-        for j in range(1, k):
-            if nums[i + j] > max:
-                max = nums[i + j]
-        arr1.append(max)
-    return arr1
+    if n == 0:
+        return 1
+        
+    elif cache is not None and cache[n] > 0:
+        return cache[n]
+    else:
+        if cache is None:
+            cache = [0 for i in range(n + 1)]
+        cache[n] = eating_cookies( n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
 
+if __name__ == "__main__":
+    # Use the main function here to test out your implementation
+    num_cookies = 5
 
-if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
-    arr = [1, 3, -1, -3, 5, 3, 6, 7]
-    k = 3
-
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,34 +3,16 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
+    output = []
 
-    result = []
+    for i in range(len(nums)-k+1):
+        output.append(max(nums[i:i+k]))
 
-    max_upto = [0] * len(nums)
-    s = [0]
-    
-    for i in range(1, len(nums)):
-        while (len(s) > 0 and nums[s[-1]] < nums[i]):
-            max_upto[s[-1]] = i - 1
-            del s[-1]
-        
-        s.append(i)
-    
-    while (len(s) > 0):
-        max_upto[s[-1]] = len(nums) - 1
-        del s[-1]
-    
-    j = 0
-    for i in range(len(nums) - k + 1):
-        while (j < i or max_upto[j] < i + k - 1):
-            j += 1
-        result.append(nums[j])
-    
-    return result
+    return output
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
